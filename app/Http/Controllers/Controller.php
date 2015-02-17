ed90567740074548bd5f78c0f3bfe50c1689f756
Edi Santoso <edicyber@gmail.com>
2015-02-17 10:35:46 +0700

    Add method input on Class Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,23 @@
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Input;
 
-abstract class Controller extends BaseController {
+abstract class Controller extends BaseController
+{
 
-	use DispatchesCommands, ValidatesRequests;
+    use DispatchesCommands, ValidatesRequests;
+
+    /**
+     * Get the input param
+     *
+     * @param $input
+     *
+     * @return mixed
+     */
+    public function input($input)
+    {
+        return Input::get($input);
+    }
 
 }
