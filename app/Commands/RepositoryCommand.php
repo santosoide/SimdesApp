9eaf1db6082de7f25b1ee792cdd208a4e88c6b5d
Edi Santoso <edicyber@gmail.com>
2015-02-18 12:11:40 +0700

    Now working with make:repository to speed up our workflow

---
+++ app/Commands/RepositoryCommand.php
@@ -0,0 +1,29 @@
+<?php namespace SimdesApp\Commands;
+
+use SimdesApp\Commands\Command;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class RepositoryCommand extends Command implements SelfHandling {
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		//
+	}
+
+}
---
+++ app/Console/Commands/RepositoryCommand.php
@@ -0,0 +1,68 @@
+<?php namespace SimdesApp\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+class RepositoryCommand extends GeneratorCommand
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:repository';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Repository class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Repository class';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('plain')) {
+            return __DIR__ . '/stubs/repository.plain.stub';
+        }
+
+        return __DIR__ . '/stubs/repository.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Repositories';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['plain', null, InputOption::VALUE_NONE, 'Generate an empty controller class.'],
+        ];
+    }
+
+}
