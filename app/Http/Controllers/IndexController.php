99a3f5134b10aa90041be9d1035a739c13f85a71
Edi Santoso <edicyber@gmail.com>
2015-03-05 19:52:43 +0700

    Fix missing routing
    - add front routing
    - fix logout routing

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,15 +1,25 @@
 <?php namespace SimdesApp\Http\Controllers;
 
-class IndexController extends Controller {
+class IndexController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
+    /**
+     * Show the back office index page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
         return view('index');
-	}
+    }
 
+    /**
+     * Show the front office index page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function front()
+    {
+        return view('front/index');
+    }
 }
