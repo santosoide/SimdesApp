fcbe182d8b072dc83b7da48848e6bacbde7d9ac6
Bagoesz <alfa.bagoesz@gmail.com>
2015-03-10 01:18:22 +0700

    Revisi Interface Dana Desa By Desa

---
+++ app/Repositories/Contracts/DanaDesaByDesaInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bagoesz
+ * Date: 10/03/2015
+ * Time: 1:04
+ */
+
+namespace SimdesApp\Repositories\Contracts;
+
+
+class DanaDesaByDesaInterface {
+
+}
